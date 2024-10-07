18b75fc924c16566806a24dfc4042a60e0a933e9
israel <isramavu@gmail.com>
2024-10-07 20:56:41 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@babel/runtime': '@babel/runtime',
-    },
+  build: {
+    outDir: 'dist',
   },
-  optimizeDeps: {
-    include: ['@babel/runtime/helpers/extends'],
-  },
-  server: {
-    host: '0.0.0.0', // Permet à Vite d'être accessible depuis le réseau local
-    port: 3000,      // Optionnel: définit le port (3000 est par défaut)
-  }
 });
